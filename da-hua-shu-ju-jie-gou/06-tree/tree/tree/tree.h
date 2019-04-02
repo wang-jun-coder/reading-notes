85a9b27abca0c6905b08f48ac346263cd716a2b1
王俊 <1127132348@qq.com>
2019-04-02 21:23:41 +0800

    二叉树性质

--- da-hua-shu-ju-jie-gou/06-tree/tree/tree/tree.h
+++ da-hua-shu-ju-jie-gou/06-tree/tree/tree/tree.h
@@ -70,4 +70,11 @@ typedef struct CSNode {
     struct CSNode, *firstChild, *rightsib;
 } CSNode, *CSTree;
 
+
+/* 二叉树的二叉链表结点结构定义*/
+typedef struct BiTNode {
+    TElemType data;                     // 结点数据
+    struct BiTNode *lchild, *rchild;    // 左右孩子指针
+} BiTNode, *BiTree;
+
 #endif /* tree_h */
